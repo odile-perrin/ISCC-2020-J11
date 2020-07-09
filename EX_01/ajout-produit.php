32cf19a57ec3425f7ea3bd6ccec0d6323c61d0f8
odpr <67262187+odpr@users.noreply.github.com>
2020-07-09 11:54:38 +0200

    Push 1

---
+++ EX_01/ajout-produit.php
@@ -0,0 +1,23 @@
+<?php
+function connect_to_database(){
+    $servername = "localhost";
+    $username = 'root';
+    $password = '';
+    $databasename = "basetest01";
+
+try{
+    $pdo = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connexion réussie";
+
+    $sql="INSERT INTO produit(id, nom, description, prix, quantite)
+    VALUES (8, 'T-shirt noir', 'T-shirt coton de couleur noire', 15.50, 10)";
+    $pdo->exec($sql);
+}
+catch (PDOException $e){
+    echo "Echec de connexion : ". $e->getMessage();
+}
+}
+connect_to_database();
+?>
